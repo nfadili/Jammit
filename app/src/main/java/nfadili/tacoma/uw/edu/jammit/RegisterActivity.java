ee1251ec8b7bc689723af49d990b57fb4bd7d39e
nfadili <nfadili@uw.edu>
2016-04-28 14:08:25 -0700

    Created a register activity screen and a button in the login activity to get there from.

---
+++ app/src/main/java/nfadili/tacoma/uw/edu/jammit/RegisterActivity.java
@@ -0,0 +1,14 @@
+package nfadili.tacoma.uw.edu.jammit;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
