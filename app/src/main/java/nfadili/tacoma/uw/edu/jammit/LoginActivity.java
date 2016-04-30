6783eac92c163faf89488a840749041241c1c8e4
nfadili <nfadili@uw.edu>
2016-04-30 11:53:48 -0700

    Gracefully handles invalid logins. Successful logins just print to console. Removed android studio generated code for reading phone contacts.

--- app/src/main/java/nfadili/tacoma/uw/edu/jammit/LoginActivity.java
+++ app/src/main/java/nfadili/tacoma/uw/edu/jammit/LoginActivity.java
@@ -47,7 +47,7 @@ import static android.Manifest.permission.READ_CONTACTS;
 /**
  * A login screen that offers login via login_email_text/login_password_text.
  */
-public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+public class LoginActivity extends AppCompatActivity {
 
     private static final String LOGIN_URL
             = "http://cssgate.insttech.washington.edu/~_450atm1/Android/login.php";
@@ -195,60 +195,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         }
     }
 
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return new CursorLoader(this,
-                // Retrieve data rows for the device user's 'profile' contact.
-                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-
-                // Select only login_email_text addresses.
-                ContactsContract.Contacts.Data.MIMETYPE +
-                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-                                                                     .CONTENT_ITEM_TYPE},
-
-                // Show primary login_email_text addresses first. Note that there won't be
-                // a primary login_email_text address if the user hasn't specified one.
-                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-        List<String> emails = new ArrayList<>();
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            emails.add(cursor.getString(ProfileQuery.ADDRESS));
-            cursor.moveToNext();
-        }
-
-        addEmailsToAutoComplete(emails);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> cursorLoader) {
-
-    }
-
-    private interface ProfileQuery {
-        String[] PROJECTION = {
-                ContactsContract.CommonDataKinds.Email.ADDRESS,
-                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
-        };
-
-        int ADDRESS = 0;
-        int IS_PRIMARY = 1;
-    }
-
-
-    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(LoginActivity.this,
-                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
-        mEmailView.setAdapter(adapter);
-    }
-
     /**
      * Switch to RegisterActivity for creating a new user account.
      * @param view
@@ -301,16 +247,17 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // Displays result info. For debugging
             if (result != null) {
                 Log.e("", result.toString());
-                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG)
-                        .show();
             }
             // Everything is good, allow access!
             if (account.getAuthenticated()) {
                 Log.e("LoginActivity", "User authenticated!");
+                showProgress(false);
                 //TODO: Switch to main screen and pass the UserAccount object!
             }
             else {
-                //TODO: Prompt user to try again.
+                showProgress(false);
+                Toast.makeText(getApplicationContext(), "Invalid credentials", Toast.LENGTH_LONG)
+                        .show();
             }
         }
 
