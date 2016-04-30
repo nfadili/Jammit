a9cfe80e57c845fc85f6244db5a550c5974de84c
nfadili <nfadili@uw.edu>
2016-04-29 17:27:40 -0700

    Upgrade to android studio 2.1

--- app/src/main/java/nfadili/tacoma/uw/edu/jammit/LoginActivity.java
+++ app/src/main/java/nfadili/tacoma/uw/edu/jammit/LoginActivity.java
@@ -50,7 +50,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
+            "nfadili@uw.edu:testing123", "rigratz@uw.edu:testingtesting"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
