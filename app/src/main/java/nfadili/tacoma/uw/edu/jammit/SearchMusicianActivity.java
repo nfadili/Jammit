5c12a7d6bea86701876427dee29ec885703e6ef9
nfadili <nfadili@uw.edu>
2016-05-04 21:02:39 -0700

    Profile table querying works when a search is made. All profiles are pulled and stored in a JSON array.

--- app/src/main/java/nfadili/tacoma/uw/edu/jammit/SearchMusicianActivity.java
+++ app/src/main/java/nfadili/tacoma/uw/edu/jammit/SearchMusicianActivity.java
@@ -1,11 +1,20 @@
 package nfadili.tacoma.uw.edu.jammit;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class SearchMusicianActivity extends AppCompatActivity {
 
@@ -33,4 +42,5 @@ public class SearchMusicianActivity extends AppCompatActivity {
             }
         });
     }
+
 }
